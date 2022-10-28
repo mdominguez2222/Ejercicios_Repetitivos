8d52fd8800968cf8404643803a2dd7b6aa71797d
Minerva <al_mpacod@iessierradelasvillas.org>
2022-10-28 11:09:44 +0200

    Act 3 acabado y 4 inacabada

--- Actividad3.py
+++ Actividad3.py
@@ -1,13 +1,17 @@
 #Ejercicio 3
 #Algoritmo que pida números hasta que se introduzca un cero. Debe imprimir lasuma y la media de todos los números introducidos.
 
-num = 0
-vNumeros = []
+num = 1
+suma = 0
+contador = -1
 
-def Suma
+print ("Escribe número 0 para salir")
 
-while (num!=0):
-    num = int(input("Dime un número: \n"))
-    
-    vNumeros.append(num)
-    print(vNumeros)
+
+while (num!=0): 
+    num = int(input("Dime un número: \n"))  
+    suma = suma + num
+    contador= contador+1
+             
+print (f"La suma es {suma}")
+print (f"La media es {suma/contador}")
