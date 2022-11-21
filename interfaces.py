c3a9654aeb57139b4f93a28d1f46c5b8637fec9c
Minerva <al_mpacod@iessierradelasvillas.org>
2022-11-21 11:14:39 +0100

    interfaz cambios

--- interfaces.py
+++ interfaces.py
@@ -1,24 +1,39 @@
 from tkinter import *
 from tkinter import ttk
 
+def saludar():
+    texto = campoTexto.get()
+    textoLabel.set(texto)
+
+
 #Generar la ventana
 ventana = Tk()
 ventana.title("Primer ejercicio")
 ventana.geometry("250x300")
 ventana.resizable(width=False,height=False)
 ventana.config(background="aquamarine")
-ventana.iconbitmap("")
+
 
 #Genera el lienzo para pintar componentes
 frm = ttk.Frame(ventana).pack()
 
 
 #Componentes Label y Button
-LabelTexto = ttk.Label(frm, text="Hola Tkinter")
+textoLabel = StringVar()
+textoLabel.set("Hola Tkinter")
+LabelTexto = ttk.Label(frm, textvariable=textoLabel)
 LabelTexto.config(background="yellow", border=2, font=("Times New Roman",15))
 LabelTexto.pack()
 
+#Componente cuadro de texto
+campoTexto = ttk.Entry(frm)
+campoTexto.pack()
+
+
+
+ttk.Button(frm, text="Saludos", command=saludar).pack()
 ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
+
 
 
 ventana.mainloop()
